060705a13c44fd78f1f8721232270fe029ab7d27
cntoplolicon <cntoplolicon@gmail.com>
2015-10-15 11:36:14 +0800

    send notification asynchronizely

--- app.rb
+++ app.rb
@@ -5,11 +5,10 @@ require 'net/http'
 require './settings'
 
 require 'byebug' if Sinatra::Base.development?
+
 Dir['./models/*.rb'].each do |f|
   require f
 end
 Dir['./controllers/*.rb'].each do |f|
   require f
 end
-
-Time.zone = 'Beijing'
--- controllers/notification.rb
+++ controllers/notification.rb
@@ -1,18 +1,20 @@
 require 'net/http'
 
 def publish_notification(channel, content)
-  uri = URI(settings.yunba[:api_uri])
-  req = Net::HTTP::Post.new(uri)
-  req['Content-Type'] = 'application/json'
-  payload = {method: 'publish', appkey: settings.yunba[:app_key],
-             seckey: settings.yunba[:secret_key], topic: channel, msg: content}
-  req.body = payload.to_json
-  Net::HTTP.start(uri.hostname, uri.port) do |http|
-    http.request(req)
+  Thread.new do
+    uri = URI(settings.yunba[:api_uri])
+    req = Net::HTTP::Post.new(uri)
+    req['Content-Type'] = 'application/json'
+    payload = {method: 'publish', appkey: settings.yunba[:app_key],
+               seckey: settings.yunba[:secret_key], topic: channel, msg: content}
+    req.body = payload.to_json
+    Net::HTTP.start(uri.hostname, uri.port) do |http|
+      http.request(req)
+    end
   end
 end
 
 def build_notification_content(user_id, type, message)
   user_id ||= 0
-  {user_id: user_id, type: type, publish_time: Time.zone.now, content: message}.to_json
+  {user_id: user_id, type: type, publish_time: Time.now, content: message}.to_json
 end
