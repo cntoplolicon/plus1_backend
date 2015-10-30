c62311a8ff41fe51858f010db21f9e5b31f55020
cntoplolicon <cntoplolicon@gmail.com>
2015-10-30 10:40:45 +0800

    jbuilder integration

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'sinatra/json'
 require 'sinatra/config_file'
+require 'sinatra/jbuilder'
 require 'net/http'
 require 'byebug' if Sinatra::Base.development?
 
@@ -19,10 +20,10 @@ end
 Time.zone_default = Time.find_zone('Beijing')
 
 get '/' do
-    send_file File.join(settings.public_folder, 'index.html')
+  send_file File.join(settings.public_folder, 'index.html')
 end
 
 get '/app_info' do
-  json version_code: '1',
-       image_hosts: [settings.s3[:host]]
+  @app_info = {version_code: 1}
+  jbuilder :app_info
 end
---
+++ views/app_info.jbuilder
@@ -0,0 +1,1 @@
+json.(@app_info, :version_code)
