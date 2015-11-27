7c9aad5d83c357fca19b27957b87881d2d247776
jiewei1988 <499155526@qq.com>
2015-11-27 15:22:02 +0800

    logic change and remove background png

--- public/js/components/appMobile.jsx
+++ public/js/components/appMobile.jsx
@@ -1,36 +1,34 @@
 const React = require('react')
+const $ = require('jquery')
 
-const WeChatBackground = React.createClass({
+module.exports = React.createClass({
   isWechat: function() {
-    var ua = navigator.userAgent.toLowerCase();
+    var ua = navigator.userAgent.toLowerCase()
     return /micromessenger/i.test(ua)
   },
 
-  render: function() {
-    if (!this.isWechat()) {
-      return null;
+  handleDownload: function(e) {
+    if(this.isWechat()) {
+      e.preventDefault()
+      $('.wechat-background').show()
+    } else {
+      $('.wechat-background').hide()
     }
-    return(
-      <div>
-        <img className="wechat-background" src="../images/wechat_background.png" />
-      </div>
-    )
-  }
-})
+  },
 
-module.exports = React.createClass({
   render: function() {
     return (
       <div>
-        <WeChatBackground/>
+        <div className="wechat-background">
+          <img className="wechat-prompt" src="../images/wechat_prompt.png" />
+        </div>
         <div className="page">
           <img className="block" width="100%" src="../images/mobile_background.png" />
-          <a href="http://download.oneplusapp.com/plus-one.apk">
+          <a onClick={this.handleDownload} href="http://download.oneplusapp.com/plus-one.apk">
             <img className="btn0" src="../images/mobile_android.png" />
           </a>
         </div>
       </div>
     )
   }
-
 })
