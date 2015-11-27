00fbecf6fe60ec4c6c5fa9b07f2259be9a568f90
jiewei1988 <499155526@qq.com>
2015-11-27 11:54:10 +0800

    prompt for wechat browser

--- public/js/components/appMobile.jsx
+++ public/js/components/appMobile.jsx
@@ -1,15 +1,34 @@
 const React = require('react')
 
+const WeChatBackground = React.createClass({
+  isWechat: function() {
+    var ua = navigator.userAgent.toLowerCase();
+    return /micromessenger/i.test(ua)
+  },
+
+  render: function() {
+    if (!this.isWechat()) {
+      return null;
+    }
+    return(
+      <div>
+        <img className="wechat-background" src="../images/wechat_background.png" />
+      </div>
+    )
+  }
+})
+
 module.exports = React.createClass({
   render: function() {
     return (
       <div>
-          <div className="page">
-            <img className="block" width="100%" src="../images/mobile_background.png" />
-            <a href="http://download.oneplusapp.com/plus-one.apk">
-              <img className="btn0" src="../images/mobile_android.png" />
-            </a>
-          </div>
+        <WeChatBackground/>
+        <div className="page">
+          <img className="block" width="100%" src="../images/mobile_background.png" />
+          <a href="http://download.oneplusapp.com/plus-one.apk">
+            <img className="btn0" src="../images/mobile_android.png" />
+          </a>
+        </div>
       </div>
     )
   }
