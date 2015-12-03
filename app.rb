da8981ef71715c193acde330bffc7534ca272633
jiewei1988 <499155526@qq.com>
2015-12-03 16:11:45 +0800

    dail with share post in ruby

--- app.rb
+++ app.rb
@@ -25,14 +25,6 @@ Rabl.configure do |config|
   config.include_child_root = false
 end
 
-helpers do
-  def image_url(path)
-    return nil unless path
-    return path if path.start_with?('http')
-    settings.cdn[:hosts].sample + path
-  end
-end
-
 def success
   json(status: 'success')
 end
@@ -52,5 +44,45 @@ get '/app_release/android' do
     json @app_release
   else
     json version_code: 0
+  end
+end
+
+helpers do
+  def image_url(path)
+    return nil unless path
+    return path if path.start_with?('http')
+    settings.cdn[:hosts].sample + path
+  end
+
+  def name_color(gender)
+    case gender
+    when 1
+      return '#5a96f0'
+    when 2
+      return '#ff62b8'
+    else
+      return '#cccccc'
+    end
+  end
+
+  def name_icon(gender)
+    case gender
+    when 1
+      return '../images/icon_man.png'
+    when 2
+      return '../images/icon_woman.png'
+    end
+  end
+
+  def time_ago(date)
+    time_ago = Time.now - date
+    return (time_ago/1.days).to_i.to_s + '天前' if (time_ago/1.days).to_i > 0
+    return (time_ago/1.hours).to_i.to_s + '小时前' if (time_ago/1.hours).to_i > 0
+    return (time_ago/1.minutes).to_i.to_s + '分钟前' if (time_ago/1.minutes).to_i > 0
+    return '刚刚'
+  end
+
+  def sort_comment(comments)
+    return comments
   end
 end
