6bff7367139ad54a855930f652d18110abc04a57
caodooGMT <496555782@qq.com>
2015-12-03 14:21:08 +0800

    add logo width and height

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151202060812) do
+ActiveRecord::Schema.define(version: 20151203042943) do
 
   create_table "account_infos", force: :cascade do |t|
     t.integer  "user_id",            limit: 4,   null: false
@@ -86,6 +86,8 @@ ActiveRecord::Schema.define(version: 20151202060812) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.string   "logo",        limit: 255
+    t.integer  "logo_width",  limit: 4
+    t.integer  "logo_height", limit: 4
   end
 
   create_table "feedbacks", force: :cascade do |t|
